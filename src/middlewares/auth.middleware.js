3d77d4937cc308fb66c88f5355243b8937876a6e
unknown <asthaagarwal203@gmail.com>
2022-05-02 13:03:20 +0530

    [Astha] Added: authentication token is verified for crud operation

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 import HttpStatus from 'http-status-codes';
 import jwt from 'jsonwebtoken';
-
+import dotenv from 'dotenv';
 /**
  * Middleware to authenticate if user has a valid Authorization token
  * Authorization: Bearer <token>
@@ -11,17 +11,14 @@ import jwt from 'jsonwebtoken';
  */
 export const userAuth = async (req, res, next) => {
   try {
-    let bearerToken = req.header('Authorization');
+    let bearerToken = req.headers['token'];
     if (!bearerToken)
       throw {
         code: HttpStatus.BAD_REQUEST,
         message: 'Authorization token is required'
       };
-    bearerToken = bearerToken.split(' ')[1];
-
-    const { user } = await jwt.verify(bearerToken, 'your-secret-key');
-    res.locals.user = user;
-    res.locals.token = bearerToken;
+    const user = await jwt.verify(bearerToken,process.env.SECRET_KEY);
+    req.body.userid=user.id;
     next();
   } catch (error) {
     next(error);
